fabeca0f5d4786df16e6ebddcb9fc0adfd3a33d7
Harry <harryshepherd0.0@hotmail.com>
2025-11-14 15:05:52 +0000

    Created Game Parameters

---
+++ engine/game_parameters.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+struct params
+{
+    static constexpr int window_width = 700;
+    static constexpr int window_height = 700;
+
+    static constexpr float g = -9.8f;   // gravity
+
+    static constexpr float phys_scale = 30.0f;
+    static constexpr float inv_phys_scale = 1.0f / phys_scale;
+
+    static constexpr int sub_step_count = 4;
+    static constexpr float time_step = 1.0f / 60.0f;    // 60FPS
+};
