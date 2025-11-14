e5b4b37e8164b93453b33a5d97195d35f3db0a76
Harry <harryshepherd0.0@hotmail.com>
2025-11-14 15:06:21 +0000

    Created Game System

---
+++ engine/game_system.cpp
@@ -0,0 +1,96 @@
+#include "game_system.hpp"
+#include "renderer.hpp"
+#include <iostream>
+
+std::shared_ptr<Scene> GameSystem::m_active_scene;
+
+void GameSystem::start(unsigned int w, unsigned int h, const std::string &title, const float &time_step)
+{
+    sf::RenderWindow window(sf::VideoMode({w, h}), title);
+
+    m_init();
+    Renderer::init(window);
+
+    sf::Event event;
+
+    while(window.isOpen())
+    {
+        static sf::Clock clock;
+        float dt = clock.restart().asSeconds();
+
+        while(window.pollEvent(event))
+        {
+            if(event.type == sf::Event::Closed)
+            {
+                window.close();
+                clean();
+                return;
+            }
+        }
+
+        if(sf::Keyboard::isKeyPressed(sf::Keyboard::Escape))
+        {
+            window.close();
+        }
+        window.clear();
+        m_update(dt);
+        m_render();
+
+        // V-sync
+        sf::sleep(sf::seconds(time_step));
+
+        window.display();
+    }
+
+    window.close();
+    clean();
+}
+
+void GameSystem::setActiveScene(const std::shared_ptr<Scene> &active_sc)
+{
+    m_active_scene = active_sc;
+}
+
+void GameSystem::m_init()
+{
+    return;
+}
+
+void GameSystem::clean()
+{
+    m_active_scene->unload();
+}
+
+void GameSystem::m_update(const float &dt)
+{
+    m_active_scene->update(dt);
+    Renderer::update(dt);
+}
+
+void GameSystem::m_render()
+{
+    m_active_scene->render();
+    Renderer::render();
+}
+
+// ---- SCENE ----
+void Scene::update(const float &dt)
+{
+    //for(std::shared_ptr<Entity> &ent : m_entities.list)
+    //{
+    //    ent->update(dt);
+    //}
+}
+
+void Scene::render()
+{
+    //for(std::shared_ptr<Entity> &ent : m_entities.list)
+    //{
+    //    ent->render();
+    //}
+}
+
+void Scene::unload()
+{
+    //m_entities.list.clear();
+}
---
+++ engine/game_system.hpp
@@ -0,0 +1,38 @@
+#pragma once
+#include "ecm.hpp"
+#include <memory>
+#include <vector>
+#include <SFML/Graphics.hpp>
+
+class Scene
+{
+public:
+    Scene() = default;
+
+    virtual ~Scene() = default;
+    virtual void update(const float &dt);
+    virtual void render();
+    virtual void load() = 0;
+    virtual void unload();
+
+    std::vector<std::shared_ptr<Entity>> &getEntities() { return m_entities.list; }
+
+protected:
+    EntityManager m_entities;
+};
+
+class GameSystem
+{
+public:
+    static void start(unsigned int w, unsigned int h, const std::string &title, const float &time_step);
+    static void clean();
+    static void reset();
+    static void setActiveScene(const std::shared_ptr<Scene>& active_sc);
+
+private:
+    static void m_init();
+    static void m_update(const float &dt);
+    static void m_render();
+    static std::shared_ptr<Scene> m_active_scene;
+
+};
