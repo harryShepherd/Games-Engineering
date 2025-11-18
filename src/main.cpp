c32740783eb5448dde53f66921bf5c4a1b4f5254
Harry <harryshepherd0.0@hotmail.com>
2025-11-18 12:52:46 +0000

    Fixes

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,18 @@
 #include <SFML/Graphics.hpp>
-#include "game_system.hpp"
 #include "game_parameters.hpp"
+#include "physics.hpp"
+#include "scenes.hpp"
 
-int main() {
-	GameSystem::start(params::window_width, params::window_height, "Cube Zone", params::time_step);
+int main() 
+{
+	Physics::initialise();
+
+	Scenes::testScene = std::make_shared<TestScene>();
+	Scenes::testScene->load();
+	
+	GameSystem::setActiveScene(Scenes::testScene);
+	GameSystem::start(params::window_width, params::window_height, "Test", Physics::time_step, true);
+
+	Physics::shutdown();
 	return 0;
 }
