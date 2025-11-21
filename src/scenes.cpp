c518607cb28216d25826cd1e52d924cacc58393f
Cameron <cameron.smith2814@gmail.com>
2025-11-21 14:14:55 +0000

    Removed reference to as yet unused branch project

--- src/scenes.cpp
+++ src/scenes.cpp
@@ -37,7 +37,7 @@ void TestScene::unload()
 
 void MenuScene::update(const float& dt) {
     if (sf::Keyboard::isKeyPressed(sf::Keyboard::Num1)) {
-        GameSystem::setActiveScene(Scenes::steeringScene);
+        
     }
     Scene::update(dt);
 }
