e74f78979a417a0a29d509916ee6e1f738fbd983
Cameron <cameron.smith2814@gmail.com>
2025-11-27 13:32:12 +0000

    Sets the window to be the size of the screen minus taskbar

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 #include "game_parameters.hpp"
 #include "physics.hpp"
 #include "scenes.hpp"
+#include <Windows.h>
 
 int main() 
 {
@@ -10,7 +11,19 @@ int main()
 	Scenes::menuScene = std::make_shared<MenuScene>();
 	Scenes::menuScene->load();
 	GameSystem::setActiveScene(Scenes::menuScene);
-	GameSystem::start(params::window_width, params::window_height, "Menu", Physics::time_step, false);
+
+	SetProcessDPIAware();
+	HWND window = GetDesktopWindow();
+	RECT r;
+	int width = params::window_height;
+	int height = params::window_width;
+	if (GetClientRect(window, &r))
+	{
+		width = r.right;
+		height = r.bottom;
+	}
+
+	GameSystem::start(width, height, "Menu", Physics::time_step, true);
 
 	Physics::shutdown();
 	return 0;
