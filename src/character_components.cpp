60774a4acb3bd3749b45dd83c29f9d5cf156bdd1
Harry <harryshepherd0.0@hotmail.com>
2025-12-05 16:44:28 +0000

    Changed kill

--- src/character_components.cpp
+++ src/character_components.cpp
@@ -4,9 +4,7 @@
 void HealthComponent::kill()
 {
     std::cout << "I am dead." << std::endl;
-    m_parent->set_alive(false);
-    m_parent->set_visible(false);
-    m_parent->set_position(sf::Vector2f(0, 0));
+    m_parent->set_to_delete();
 }
 
 float HealthComponent::get_current_health() { return current_health; }
@@ -16,8 +14,6 @@ float HealthComponent::get_max_health() { return max_health; }
 void HealthComponent::take_damage(const float dmg)
 {
     current_health -= dmg;
-
-    std::cout << "I took " << dmg << " damage and have " << current_health << " health remaining." << std::endl;
     
     // we are dead.
     if(current_health <= 0.0f) 
@@ -39,16 +35,9 @@ void HealthComponent::regain_health(const float health)
 
 HealthComponent::HealthComponent(Entity *p, float health) :
     Component(p), max_health(health), current_health(health)
-{
-    std::cout << "I have " << current_health << " health" << std::endl;
-}
+{}
 
 void HealthComponent::update(const float& dt)
-{
-    if(sf::Keyboard::isKeyPressed(sf::Keyboard::E))
-    {
-        take_damage(2.0f);
-    }
-};
+{}
 
 void HealthComponent::render() {}
